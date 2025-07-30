c7dff819a280eb8e622d2d486c8588847cdb97a0
Ower A Campos <owerion22@gmail.com>
2025-07-30 14:26:39 -0500

    refactor(fetchData): update consult
    is updated the callback a the function at get data something according to the selected

--- Frontend/src/hooks/useFetchData.ts
+++ Frontend/src/hooks/useFetchData.ts
@@ -1,21 +1,46 @@
 import { useEffect, useState } from 'react';
 import { useDashboard } from '../context/DashboardContext';
-import { googleServices } from '../services/GoogleServices';
+import { TypeOfConsultServices } from '../services/TypeOfConsultServices';
+import { googleCustomerServices, googleDateSettlementServices } from '../services/GoogleServices';
 
 const useFetchData = (widgetId: string) => {
   const { state } = useDashboard();
-  const [data, setData] = useState<any>([]);
+  const [dateSettlement, setDateSettlement] = useState<any>([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = () => {
       try {
-        let info = googleServices();
-        info.then((response: any) => {
-          setData(response);
-        })
+        const search: string = TypeOfConsultServices(state.widgets, widgetId);
+
+        switch (search) {
+          case 'fechaRadicacion':
+            let dateSettlement = googleDateSettlementServices();
+            dateSettlement.then((response: any) => {
+              setDateSettlement(response);
+            });
+            break;
+          case 'clientes':
+            let Customers = googleCustomerServices();
+            Customers.then((response: any) => {
+              console.warn(response);
+            })
+            break;
+          case 'sede':
+            break;
+          case 'tipoNovedad':
+            break;
+          case 'sedeResponsable':
+            break;
+          case 'procesoResponsable':
+            break;
+          case 'tratamientoNovedad':
+            break;
+          case 'tiempoRespuesta':
+            break;
+        }
       } catch (error) {
-        console.log('Error fetching data:', error);
+        console.error('Error fetching data:', error);
       } finally {
         setLoading(false);
       }
@@ -25,7 +50,7 @@ const useFetchData = (widgetId: string) => {
     return () => { };
   }, [widgetId, state.filters, state.dateRange]);
 
-  return { data, loading };
+  return { dateSettlement, loading };
 };
 
 export default useFetchData;
