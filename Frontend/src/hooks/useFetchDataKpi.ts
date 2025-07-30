a988213f88d43ea064152e5965e45a0a853d9b16
Ower A Campos <owerion22@gmail.com>
2025-07-30 14:27:02 -0500

    refactor(fetchData): update consult
    is updated the callback a the function at get data something according to the selected

--- Frontend/src/hooks/useFetchDataKpi.ts
+++ Frontend/src/hooks/useFetchDataKpi.ts
@@ -1,27 +1,47 @@
 import { useEffect, useState } from 'react';
 import { useDashboard } from '../context/DashboardContext';
-import { googleServices } from '../services/GoogleServices';
+import { googleDateSettlementServices } from '../services/GoogleServices';
+import { TypeOfConsultServices } from '../services/TypeOfConsultServices';
 
 const useFetchDataKpi = (widgetId: string) => {
   const { state } = useDashboard();
-  const [data, setData] = useState<any>([]);
+  const [dateSettlement, setDateSettlement] = useState<any>([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = () => {
       try {
-        let info = googleServices();
-        info.then((response: any) => {
-          const current_month = new Date().getMonth();
-          const current_year = new Date().getFullYear();
-          const data_for_graphKPI = {
-            currentValue: response[current_month][current_year],
-            previousValue: response[current_month - 1][current_year]
-          }
-          setData(data_for_graphKPI);
-        })
+        const search: string = TypeOfConsultServices(state.widgets, widgetId);
+        switch (search) {
+          case 'fechaRadicacion':
+            let info = googleDateSettlementServices();
+            info.then((response: any) => {
+              const current_month = new Date().getMonth();
+              const current_year = new Date().getFullYear();
+              const data_for_graphKPI = {
+                currentValue: response[current_month][current_year],
+                previousValue: response[current_month - 1][current_year]
+              }
+              setDateSettlement(data_for_graphKPI);
+            })
+            break;
+          case 'clientes':
+            break;
+          case 'sede':
+            break;
+          case 'tipoNovedad':
+            break;
+          case 'sedeResponsable':
+            break;
+          case 'procesoResponsable':
+            break;
+          case 'tratamientoNovedad':
+            break;
+          case 'tiempoRespuesta':
+            break;
+        }
       } catch (error) {
-        console.log('Error fetching data:', error);
+        console.error('Error fetching data:', error);
       } finally {
         setLoading(false);
       }
@@ -31,7 +51,7 @@ const useFetchDataKpi = (widgetId: string) => {
     return () => { };
   }, [widgetId, state.filters, state.dateRange]);
 
-  return { data, loading };
+  return { dateSettlement, loading };
 };
 
 export default useFetchDataKpi;
